d851fb71289558c99715e94136cbb5f737874394
shailendra pratap <singhlalprat@gmail.com>
2026-03-16 20:04:13 +0530

    requestslice and component

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  userReducer from "./UserSlice";
 import feedReducer from "./feedSlice";
-
-
+import connectionReducer from "./connectionSlice"
+import requestReducer from "./requestSlice";
 const appStore = configureStore({
     reducer:{ 
         user:userReducer,
         feed:feedReducer,
+         connection : connectionReducer,
+    request: requestReducer,
     },
 })
 
